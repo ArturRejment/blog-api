e84488bfeb0e9d4c72ecaae6b3f57c330d1d54bc
Artur Rejment <arturrejment1@gmail.com>
2021-07-18 19:47:54 +0200

    CRUD functionality for blog posts

--- api/Views/posts.py
+++ api/Views/posts.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 
 import api.models as ApiModels
 import api.serializers as ApiSerializers
@@ -17,3 +18,48 @@ class PostView(APIView):
 			return Response("Post created successfully", status=201)
 		else:
 			return Response(serializer.errors, status=422)
+
+	def get(self, request):
+		""" Return set of all posts """
+
+		posts = ApiModels.Post.objects.all()
+		serializer = ApiSerializers.PostSerializer(posts, many=True)
+		return Response(serializer.data, status=200)
+
+	def put(self, request):
+		""" Change existing post
+
+			Required params:
+			@param1 - postID """
+
+		postID = request.data.get('postID')
+		try:
+			post = ApiModels.Post.objects.get(id=postID)
+		except Exception as e:
+			raise ValidationError(e)
+
+		serializer = ApiSerializers.PostSerializer(instance=post, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=200)
+		else:
+			return Response(serializer.errors, status=422)
+
+	def delete(self, request):
+		""" Delete post
+
+			Required params:
+			@param1 - postID"""
+
+		postID = request.data.get('postID')
+		try:
+			post = ApiModels.Post.objects.get(id=postID)
+		except Exception as e:
+			raise ValidationError(e)
+
+		try:
+			post.delete()
+		except Exception as e:
+			return Response(e)
+		else:
+			return Response("Post deleted", status=200)
--- api/tests/test_post.py
+++ api/tests/test_post.py
@@ -6,19 +6,30 @@ class TestPostEndpoints(APITestCase):
 	""" Testing some basic Post Endpoints """
 
 	def setUp(self):
-		user1 = ApiModels.User.objects.create(
+		""" Set up Post tests """
+
+		# Create one user for tests
+		self.user1 = ApiModels.User.objects.create(
 			username = 'test',
 			first_name='Test',
 			last_name='Test',
 			email='test@gmail.com'
 		)
 
+		# Create new post
+		self.post1 = ApiModels.Post.objects.create(
+			author=self.user1,
+			title='New',
+			content='Hi'
+		)
+
 	def test_post_POST(self):
+		""" Test POST method """
 
 		response = self.client.post(
 			'/post/',
 			{
-				'author': 2,
+				'author': self.user1.id,
 				'title': 'New tech',
 				'content': 'Hello'
 			},
@@ -28,3 +39,59 @@ class TestPostEndpoints(APITestCase):
 		)
 
 		self.assertEquals(response.status_code, 201)
+
+	def test_post_GET(self):
+		""" Test GET method """
+
+		response = self.client.get(
+			'/post/'
+		)
+
+		# There should be no posts
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(len(response.data), 1)
+
+		# Create new post
+		self.test_post_POST()
+
+		response = self.client.get(
+			'/post/'
+		)
+
+		# There should be one post
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(len(response.data), 2)
+
+	def test_post_PUT(self):
+		""" Test PUT method """
+
+		response = self.client.put(
+			'/post/',
+			{
+				'postID': self.post1.id,
+				'author': self.user1.id,
+				'title': 'New technology',
+				'content': 'Hello there'
+			},
+			headers={
+				'Content-Type':'application/x-www-form-urlencoded'
+			}
+		)
+
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(response.data.get('title'), 'New technology')
+
+	def test_post_DELETE(self):
+		""" Test DELETE method """
+
+		response = self.client.delete(
+			'/post/',
+			{
+				'postID': self.post1.id
+			},
+			headers={
+				'Content-Type':'application/x-www-form-urlencoded'
+			}
+		)
+
+		self.assertEquals(response.status_code, 200)
