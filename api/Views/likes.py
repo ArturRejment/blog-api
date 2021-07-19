b5fd2a75f3759314bd04fb9827d5b7442bf25cf9
Artur Rejment <arturrejment1@gmail.com>
2021-07-19 23:56:23 +0200

    User authentication

--- api/Views/likes.py
+++ api/Views/likes.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.serializers import ValidationError
 
 import api.models as ApiModels
@@ -18,3 +18,21 @@ def viewLikesForPost(request, **kwargs):
 	likes = post.postlike_set.all()
 	serializer = ApiSerializers.PostLikeSerializer(likes, many=True)
 	return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def likePost(request, **kwargs):
+	user = request.user
+	postID = kwargs['id']
+	try:
+		post = ApiModels.Post.objects.get(id=postID)
+	except Exception as e:
+		raise ValidationError(e)
+
+	if post in user.post_set.all():
+		raise ValidationError('User already likes this post', status=422)
+
+	ApiModels.PostLike.objects.create(
+		user=user,
+		post=post
+	)
--- api/urls.py
+++ api/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 	path('post/<int:id>/', PostView.PostDetailView.as_view(), name='post'),
 	path('post/<int:id>/comments/', CommentsView.CommentDetailView.as_view(), name='post_comment'),
 	path('post/<int:id>/likes/', LikesView.viewLikesForPost, name='likes_for_post'),
+	path('post/<int:id>/like/', LikesView.likePost, name='like_post'),
 	path('comments/', CommentsView.CommentView.as_view(), name='comments')
 ]
