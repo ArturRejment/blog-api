a9d55ab36c546aa915a3e8b4c18f9e82ea6efd9f
Artur Rejment <arturrejment1@gmail.com>
2021-07-20 14:23:46 +0200

    Serializing User

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 import api.models as ApiModels
+from djoser.serializers import UserCreateSerializer, UserSerializer
+
+class UserSerializer(UserSerializer):
+	class Meta(UserSerializer.Meta):
+		model = ApiModels.User
+		fields = ['username', 'first_name', 'last_name', 'imageURL']
 
 class PostSerializer(serializers.ModelSerializer):
+	author = UserSerializer()
 	class Meta:
 		model = ApiModels.Post
 		fields = ['id', 'author', 'title', 'content', 'number_of_likes', 'imageURL']
@@ -12,11 +19,13 @@ class CommentSerializer(serializers.ModelSerializer):
 		fields = ['id', 'post', 'username', 'content']
 
 class PostLikeSerializer(serializers.ModelSerializer):
+	user = UserSerializer()
 	class Meta:
 		model = ApiModels.PostLike
 		fields = ['user']
 
 class CommentLikeSerializer(serializers.ModelSerializer):
+	user = UserSerializer()
 	class Meta:
 		model = ApiModels.CommentLike
 		fields = ['user']
