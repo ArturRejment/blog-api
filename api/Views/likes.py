2672e22863e5ed39bf8fb71edae90f96225f5c9a
Artur Rejment <arturrejment1@gmail.com>
2021-07-24 13:11:13 +0200

    Adding comments to favoites functionality

--- api/Views/likes.py
+++ api/Views/likes.py
@@ -7,42 +7,79 @@ from rest_framework.exceptions import NotFound
 
 import api.models as ApiModels
 import api.serializers as ApiSerializers
-from api.renderers import PostJSONRenderer
+from api.renderers import PostJSONRenderer, CommentJSONRenderer
 
 class ArticlesFavoriteAPIView(APIView):
-    permission_classes = (IsAuthenticated,)
-    renderer_classes = (PostJSONRenderer,)
-    serializer_class = ApiSerializers.PostSerializer
+	""" Views for favorite and unfavorite specific post """
+	permission_classes = (IsAuthenticated,)
+	renderer_classes = (PostJSONRenderer,)
+	serializer_class = ApiSerializers.PostSerializer
 
-    def delete(self, request, id=None):
-        user = self.request.user
-        serializer_context = {'request': request}
+	def delete(self, request, id=None):
+		user = self.request.user
+		serializer_context = {'request': request}
 
-        try:
-            post = ApiModels.Post.objects.get(id=id)
-        except post.DoesNotExist:
-            raise NotFound('An article with this slug was not found.')
+		try:
+			post = ApiModels.Post.objects.get(id=id)
+		except post.DoesNotExist:
+			raise NotFound('An article with this slug was not found.')
 
-        user.unfavorite(post)
+		user.unfavorite(post)
 
-        serializer = self.serializer_class(post, context=serializer_context)
+		serializer = self.serializer_class(post, context=serializer_context)
 
-        return Response(serializer.data, status=200)
+		return Response(serializer.data, status=200)
 
-    def post(self, request, id=None):
-        user = self.request.user
-        serializer_context = {'request': request}
+	def post(self, request, id=None):
+		user = self.request.user
+		serializer_context = {'request': request}
 
-        try:
-            post = ApiModels.Post.objects.get(id=id)
-        except ApiModels.Post.DoesNotExist:
-            raise NotFound('An post with this id was not found.')
+		try:
+			post = ApiModels.Post.objects.get(id=id)
+		except ApiModels.Post.DoesNotExist:
+			raise NotFound('An post with this id was not found.')
 
-        user.favorite(post)
+		user.favorite(post)
 
-        serializer = self.serializer_class(post, context=serializer_context)
+		serializer = self.serializer_class(post, context=serializer_context)
 
-        return Response(serializer.data, status=201)
+		return Response(serializer.data, status=201)
+
+class CommentsFavoriteAPIView(APIView):
+	""" Views for favorite and unfavorite specific comment """
+	permission_classes = (IsAuthenticated,)
+	renderer_classes = (CommentJSONRenderer,)
+	serializer_class = ApiSerializers.CommentSerializer
+
+	def delete(self, request, id=None):
+		user = self.request.user
+		serializer_context = {'request': request}
+
+		try:
+			comment = ApiModels.Comment.objects.get(id=id)
+		except ApiModels.Comment.DoesNotExist:
+			raise NotFound('An post with this id was not found.')
+
+		user.unfavorite_comment(comment)
+
+		serializer = self.serializer_class(comment, context=serializer_context)
+
+		return Response(serializer.data, status=200)
+
+	def post(self, request, id=None):
+		user = self.request.user
+		serializer_context = {'request': request}
+
+		try:
+			comment = ApiModels.Comment.objects.get(id=id)
+		except ApiModels.Comment.DoesNotExist:
+			raise NotFound('An comment with this id was not found.')
+
+		user.favorite_comment(comment)
+
+		serializer = self.serializer_class(comment, context=serializer_context)
+
+		return Response(serializer.data, status=201)
 
 
 @api_view(['GET'])
@@ -70,26 +107,3 @@ def viewLikesForComment(request, **kwargs):
 	likes = comment.commentlike_set.all()
 	serializer = ApiSerializers.CommentLikeSerializer(likes, many=True)
 	return Response(serializer.data)
-
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def likeComment(request, **kwargs):
-	""" Allows to like specific comment by currently logged user """
-	user = request.user
-	commentID = kwargs['id']
-	try:
-		comment = ApiModels.Comment.objects.get(id=commentID)
-	except Exception as e:
-		raise ValidationError(e)
-
-	for i, element in enumerate(user.commentlike_set.all()):
-		if comment == element.comment:
-			raise ValidationError({'User': 'User already likes this comment'}, code=422)
-
-	ApiModels.CommentLike.objects.create(
-		user=user,
-		comment=comment
-	)
-
-	return Response("Comment liked", status=200)
