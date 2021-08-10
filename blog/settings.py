1865b06b496f5e961d8c77e7056eb7f4563e8f33
Artur Rejment <arturrejment1@gmail.com>
2021-08-10 23:16:25 +0200

    Pagination changed to 12 posts per page (Closes #16)

--- blog/settings.py
+++ blog/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 12,
 }
 
 AUTH_USER_MODEL = 'api.User'
